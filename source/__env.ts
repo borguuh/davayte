e428ebd58f78551708dc31cced04b98f19a5a05b
Elpis <elpis409@gmail.com>
2024-05-27 18:14:49 +0100

    header and footer

---
+++ source/__env.ts
@@ -0,0 +1,4 @@
+export const instagram = "https://www.instagram.com/davayte_official/";
+export const email = "mailto:davayte.app@gmail.com";
+export const twitter = "";
+export const linkedin = "";
---
+++ source/components/general/PageNotFound.tsx
@@ -0,0 +1,8 @@
+const PageNotFound = () => {
+	return (
+		<div className="flex-1 flex items-center justify-center text-center p-5">
+			<h1 className="text-3xl font-bold uppercase font-body">This page is unavailable</h1>
+		</div>
+	)
+}
+export default PageNotFound
