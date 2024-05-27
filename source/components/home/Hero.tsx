a78e9ad49a0a6d8e2ed0baedb7660cb9177c33d8
Borguuh <abdrahmanadewale@gmail.com>
2024-05-27 21:34:11 +0100

    res

--- source/components/home/Hero.tsx
+++ source/components/home/Hero.tsx
@@ -15,25 +15,25 @@ const m = {
 const Hero = () => {
 
   return (
-    <section className="w-full pt-16 flex flex-col gap-10 items-center justify-between text-center">
+    <section className="w-full pt-10 sm:pt-16 flex flex-col gap-10 items-center justify-between text-center">
       <div className="flex-1 flex flex-col items-center justify-center w-full">
         <div className="flex flex-col gap-4 w-full">
           <motion.h1 initial={m.initial} whileInView={m.whileInView} transition={{
             delay: 0,
             duration: 0.5
-          }} className="text-5xl font-semibold leading-relaxed text-balance">
+          }} className="text-3xl sm:text-4xl lg:text-5xl font-semibold text-balance" style={{ lineHeight: 1.5 }}>
             Transforming <span className="text-highlight">Event Management</span> with Unique Communities
           </motion.h1>
           <motion.p initial={m.initial} whileInView={m.whileInView} transition={{
             delay: 0.3,
             duration: 0.5
-          }} className="text-lg opacity-70">Building Unique Communities, One Event at a time</motion.p>
+          }} className="text-sm sm:text-base xl:text-lg opacity-70">Building Unique Communities, One Event at a time</motion.p>
         </div>
         <div className="w-full flex flex-col items-center justify-center pt-2">
           <motion.img initial={m.initial} whileInView={m.whileInView} transition={{
             delay: 0.6,
             duration: 0.5
-          }} src="/hero.svg" alt="Hero" className="w-[800px]" />
+          }} src="/hero.svg" alt="Hero" className="max-w-[800px] w-full" />
         </div>
       </div>
     </section>
